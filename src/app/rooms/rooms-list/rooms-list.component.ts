ce6095bed94be97209ec53ad255e4c6624901f33
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2023-01-08 17:42:01 +0100

    Added service example with getRooms

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,13 +1,30 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'hinv-rooms-list',
   templateUrl: './rooms-list.component.html',
   styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoomsListComponent {
+export class RoomsListComponent implements OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes?.['title']) {
+      this.title = changes['title'].currentValue.toUpperCase();
+    }
+  }
   @Input() rooms: RoomList[] = [];
+
+  @Input() title: string = '';
 
   @Output() selectedRoom = new EventEmitter<RoomList>();
 
