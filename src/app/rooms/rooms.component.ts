fb23d4e3aee2dcb65daa6cbae9a1d30a69e6f17c
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2023-01-08 13:03:44 +0100

    Added Angular Input and Output for component bidirectional communication

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Room, RoomList } from './rooms';
 
 @Component({
@@ -6,12 +6,49 @@ import { Room, RoomList } from './rooms';
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss'],
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
+  ngOnInit(): any {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free WiFi',
+        price: 500,
+        photos:
+          'https://www.dearhotelmadrid.com/images/CARROUSEL_THEHOTEL_3.jpg',
+        checkinTime: new Date('07-01-2023'),
+        checkoutTime: new Date('08-01-2023'),
+        rating: 4.58,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Greater Deluxe Room',
+        amenities: 'Air Conditioner, Free WiFi',
+        price: 1000,
+        photos:
+          'https://www.dearhotelmadrid.com/images/CARROUSEL_THEHOTEL_3.jpg',
+        checkinTime: new Date('07-01-2023'),
+        checkoutTime: new Date('08-01-2023'),
+        rating: 3.43,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'Air Conditioner, Free WiFi',
+        price: 15000,
+        photos:
+          'https://www.dearhotelmadrid.com/images/CARROUSEL_THEHOTEL_3.jpg',
+        checkinTime: new Date('07-01-2023'),
+        checkoutTime: new Date('08-01-2023'),
+        rating: 2.65,
+      },
+    ];
+  }
+
   hotelName = 'Hilton Hotel';
-
   numberOfRooms = 10;
-
   hideRooms = false;
+  selectedRoom = {} as RoomList;
 
   rooms: Room = {
     totalRooms: 20,
@@ -19,40 +56,15 @@ export class RoomsComponent {
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free WiFi',
-      price: 500,
-      photos: 'https://www.dearhotelmadrid.com/images/CARROUSEL_THEHOTEL_3.jpg',
-      checkinTime: new Date('07-01-2023'),
-      checkoutTime: new Date('08-01-2023'),
-      rating: 4.58,
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Greater Deluxe Room',
-      amenities: 'Air Conditioner, Free WiFi',
-      price: 1000,
-      photos: 'https://www.dearhotelmadrid.com/images/CARROUSEL_THEHOTEL_3.jpg',
-      checkinTime: new Date('07-01-2023'),
-      checkoutTime: new Date('08-01-2023'),
-      rating: 3.43,
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suite',
-      amenities: 'Air Conditioner, Free WiFi',
-      price: 15000,
-      photos: 'https://www.dearhotelmadrid.com/images/CARROUSEL_THEHOTEL_3.jpg',
-      checkinTime: new Date('07-01-2023'),
-      checkoutTime: new Date('08-01-2023'),
-      rating: 2.65,
-    },
-  ];
+  roomList: RoomList[] = [];
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(receivedRoom: RoomList) {
+    this.roomList = this.roomList.filter(
+      (room) => room.price !== receivedRoom.price
+    );
   }
 }
